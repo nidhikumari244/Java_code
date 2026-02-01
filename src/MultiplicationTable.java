92fd7b3cf4b4f99deabb6a6b21ef3d8aeabaa591
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-01 19:47:59 +0530

    add some more codes on previous

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" enter ur no: ");
+        int num = scanner.nextInt();
+        printMultiplicationTable(num);
+    }
+    public static void printMultiplicationTable ( int num){
+    int i = 0;
+    while (i <= 10){
+        System.out.println(num + "X" + i + " = " + (num*i));
+        i++;
+    }
+    }
+}
