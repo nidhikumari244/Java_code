6c549ff4fd95b4be91fd1d41a22358c4c71bd1b0
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-14 17:14:23 +0530

    Diagonal sum of 2D array

---
+++ java-fundamentals/src/DiagonalSum2DArr.java
@@ -0,0 +1,39 @@
+ class DiagonalSum2DArr {
+    public static void main(String[] args) {
+        int [][] numArr = ArrayUtility.input2DArray();
+            long sum = sumOfDiagonals(numArr);
+            System.out.println("Sum of Diagonals is: " +sum);
+
+    }
+
+    public static long sumOfDiagonals(int[][] numArr){
+        long leftSum =sumOfLeftDiagonals(numArr);
+        long rightSum =sumOfRightDiagonals(numArr);
+        long sum = leftSum +rightSum;
+        if (numArr.length % 2 != 0){
+            int ind = numArr.length /2;
+            sum -= numArr[ind][ind];
+        }
+        return sum;
+    }
+
+     public static long sumOfLeftDiagonals(int[][] numArr){
+        long sum = 0;
+        int i = 0;
+        while ( i< numArr.length) {
+            sum += numArr[i][i];
+            i++;
+        }
+         return sum;
+     }
+     public static long sumOfRightDiagonals(int[][] numArr){
+        long sum =0;
+        int i = 0;
+        while ( i < numArr.length) {
+            int col = numArr.length -1 -i;
+            sum += numArr[i][col];
+            i++;
+        }
+         return sum;
+     }
+}
