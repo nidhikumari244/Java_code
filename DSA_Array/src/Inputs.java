453249120b697683d25005dbdb16b02e07ea26f7
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-15 23:39:02 +0530

    Array for DSA

---
+++ DSA_Array/src/Inputs.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Inputs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Array of primitives
+//       int[] arr = new int[6];
+//        System.out.println(arr[4]);
+
+        // input using loop--
+//       for (int i = 0;i<arr.length;i++){
+//           arr[i]=in.nextInt();
+//
+//        }
+//        System.out.println(Arrays.toString(arr));
+
+//       for(int i = 0; i<arr.length;i++){
+//           System.out.print(arr[i] +" ");
+//       }
+
+//       for(int num : arr) {
+//           System.out.print(num + " ");
+//       }
+
+        // Array of Objects
+       String[] str = new String[4];
+       for (int i = 0; i < str.length; i++) {
+           str[i]= in.next();
+       }
+        System.out.println(Arrays.toString(str));
+    }
+}
---
+++ DSA_Array/src/Main.java
@@ -0,0 +1,35 @@
+// input ---
+
+class main {
+    public static void main(String[] args) {
+        // q : store a roll no.---
+
+        int a = 19;
+
+        //Q: store a person's roll no.---
+
+        String name = " nidhi";
+
+        int roll1 = 1;
+        int roll2 = 4;
+        int roll4 = 54;
+
+        // SYNTAX --
+        //datatype[] variable_name = new datatype[size];
+
+        // store 5 roll no.---
+        int [] rnos = new int[8];
+
+        //or
+        int[] rons2={12,3,4,5,6,1,7,8};
+        int[] ros; // declaration of array ros is getting defined in the class.
+        ros = new int[5];// initialization : obj s being created in memory (heap)
+        //System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+        String str = null;
+        System.out.println(arr[0]);
+    }
+}
