9dd597510a37a9b514d18ea7d4e1965674f85607
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-21 23:58:12 +0530

    liner search

--- Linear Search/src/Main.java
+++ Linear Search/src/Main.java
@@ -1,8 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//       int [] nums = {12,2,4,45,65,-2,14,-10};
+//       int target = 14;
+//       int ans = linerSearch(nums,target);
+//        System.out.println(ans);
+//
+//    }
+//    static boolean linerSearch(int[] arr, int target ) {
+//        if (arr.length == 0) {
+//            return false;
+//        }
+//        for (int element : arr) {
+//            if (element == target) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    static int linerSearch2(int[] arr, int target ){
+//        if (arr.length == 0) {
+//            return -1;
+//        }
+//    }
+//    for (int element :)
+//}
