e8af1b0cacfb73d6eac94fdf86577745f3e81940
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-24 10:07:57 +0530

    search in string

--- Linear Search/src/SearchInString.java
+++ Linear Search/src/SearchInString.java
@@ -1,5 +1,26 @@
 public class SearchInString {
     public static void main(String[] args) {
+        int [] arr = {1,12,24,54,65,76,87,3,100};
+        int target = 12;
+        System.out.println(linearSearch(arr, target,1,4));
 
+    }
+
+    static int linearSearch(int[] arr, int target, int start , int end) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for (int index = start; index <= end; index++) {
+            // check for the element at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+
+        }
+        // this line will execute if none of the return statements above have executed
+        // so the target not found
+        return -1;
     }
 }
