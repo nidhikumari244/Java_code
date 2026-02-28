32ee9eec1083b15207cc1d8529a899a64db7281d
nidhikumari244 <nidhigupta2462@gmail.com>
2026-02-28 22:59:24 +0530

    find min in given no.

--- Linear Search/src/findMin.java
+++ Linear Search/src/findMin.java
@@ -1,9 +1,17 @@
-//public class findMin {
-//    public static void main(String[] args) {
-//        int[] arr ={89,24,1,67,87,90,24,4,6};
-//        System.out.println(min(arr));
-//    }
-//    static int min (int[] arr) {
-//
-//    }
-//}
+public class findMin {
+    public static void main(String[] args) {
+        int[] arr ={12, 9 , 4, 5, 7,8};
+        System.out.println(min(arr));
+    }
+    static int min (int[] arr) {
+        int ans = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if (arr[i] < ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+
+
+    }
+}
